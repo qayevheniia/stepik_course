111cad2a8cc8cc106f9d93f00eeba2a45919a5ae
qayevheniia <qayevheniia@gmail.com>
2020-05-23 00:17:17 +0300

    проверка наличие конкретного слова и ли текста в тексте

---
+++ проверка на присутствие отдельного текста.py
@@ -0,0 +1,13 @@
+s = 'My Name  is Julia'
+
+if 'Name' in s:
+    print('Substring found')
+
+index = s.find('Name')
+if index != -1:
+    print(f'Substring found at index {index}')
+
+
+
+def test_substring(full_string, substring):
+    assert 'substring' in 'full_string', f" expected 'full_string' to be substring of 'substring'"
